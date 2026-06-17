6285658723799c7bbab14f3c6e8fe110641994b6
João Vitor Cleto <119801314+JohnnyCleto@users.noreply.github.com>
2026-06-17 15:26:51 -0300

    Update server.js

--- auth-service/src/server.js
+++ auth-service/src/server.js
@@ -9,9 +9,6 @@ require('./presentation/routes/authRoutes');
 const initDatabase =
 require('./infrastructure/database/initDatabase');
 
-const profileRoutes =
-require('./presentation/routes/profileRoutes');
-
 const app = express();
 
 app.use(cors());
