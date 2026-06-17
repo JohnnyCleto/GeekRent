b93f04fc9130cc6f27d021216871c04188a0681b
João Vitor Cleto <119801314+JohnnyCleto@users.noreply.github.com>
2026-06-17 17:52:58 -0300

    Update LoginUseCase.js

--- auth-service/src/application/usecases/LoginUseCase.js
+++ auth-service/src/application/usecases/LoginUseCase.js
@@ -1,9 +1,6 @@
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { jwtSecret } = require('../../config/env');
 
 class LoginUseCase {
-
   constructor(userRepository) {
     this.userRepository = userRepository;
   }
@@ -13,33 +10,20 @@ class LoginUseCase {
     const user = await this.userRepository.findByEmail(email);
 
     if (!user) {
-      throw new Error('Usuário não encontrado');
+      throw new Error("Usuário não encontrado");
     }
 
     const valid = await bcrypt.compare(password, user.password);
 
     if (!valid) {
-      throw new Error('Senha inválida');
+      throw new Error("Senha inválida");
     }
 
-    const token = jwt.sign(
-      {
-        id: user.id,
-        email: user.email,
-        role: user.role
-      },
-      jwtSecret,
-      { expiresIn: '7d' }
-    );
-
     return {
-      token,
-      user: {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        role: user.role
-      }
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role
     };
   }
 }
