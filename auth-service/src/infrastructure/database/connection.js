4af76d99fcda57274cf903077bd2e6e32e9fb94e
João Vitor Cleto <119801314+JohnnyCleto@users.noreply.github.com>
2026-06-17 13:24:46 -0300

    Update connection.js

--- auth-service/src/infrastructure/database/connection.js
+++ auth-service/src/infrastructure/database/connection.js
@@ -28,5 +28,9 @@ class DatabaseConnection {
         return DatabaseConnection.instance;
     }
 }
+console.log("DB_HOST =", process.env.DB_HOST);
+console.log("DB_PORT =", process.env.DB_PORT);
+console.log("DB_NAME =", process.env.DB_NAME);
+console.log("DB_USER =", process.env.DB_USER);
 
 module.exports = DatabaseConnection;
