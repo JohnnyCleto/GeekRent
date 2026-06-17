6ee6dc26857ed6a308acc3edc5d7f4bbaab042e0
João Vitor Cleto <119801314+JohnnyCleto@users.noreply.github.com>
2026-06-17 15:06:27 -0300

    Update authRoutes.js

--- auth-service/src/presentation/routes/authRoutes.js
+++ auth-service/src/presentation/routes/authRoutes.js
@@ -1,3 +1,5 @@
+// src/presentation/routes/authRoutes.js
+
 const express =
 require('express');
 
@@ -6,6 +8,15 @@ express.Router();
 
 const AuthController =
 require('../controllers/AuthController');
+
+const ProfileController =
+require('../controllers/ProfileController');
+
+const DashboardController =
+require('../controllers/DashboardController');
+
+const authMiddleware =
+require('../middlewares/authMiddleware');
 
 router.post(
     '/register',
@@ -17,26 +28,10 @@ router.post(
     AuthController.login
 );
 
-const express =
-require('express');
-
-const router =
-express.Router();
-
-const ProfileController =
-require('../controllers/ProfileController');
-
-const authMiddleware =
-require('../middlewares/authMiddleware');
-
 router.get(
-
     '/me',
-
     authMiddleware,
-
     ProfileController.getProfile
-
 );
 
 router.get(
