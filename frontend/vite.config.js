56ac50838fdbd6a1a3432e237901ccd3f912bd4d
João Vitor Cleto <119801314+JohnnyCleto@users.noreply.github.com>
2026-06-17 09:28:03 -0300

    Enhance Vite config with server settings
    Added server configuration with allowed hosts.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+
+  plugins:[react()],
+
+  server:{
+
+    allowedHosts:[
+
+      'natural-expression-production-2e65.up.railway.app'
+
+    ]
+
+  }
+
 })
