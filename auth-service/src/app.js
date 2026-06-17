c3d2165a8540d64644b482c2451511d1e662eabc
João Vitor Cleto <119801314+JohnnyCleto@users.noreply.github.com>
2026-06-17 11:10:32 -0300

    Update app.js

--- auth-service/src/app.js
+++ auth-service/src/app.js
@@ -11,4 +11,26 @@ app.use(express.json());
 
 app.use('/auth', authRoutes);
 
+app.get('/', (req,res)=>{
+
+res.json({
+
+service:'GeekRent Auth Service',
+
+status:'online'
+
+});
+
+});
+
+app.get('/health',(req,res)=>{
+
+res.json({
+
+status:'ok'
+
+});
+
+});
+
 module.exports = app;
