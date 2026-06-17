d507fcbb95f83a842b78a8056b77583e15a8ea33
João Vitor Cleto <119801314+JohnnyCleto@users.noreply.github.com>
2026-06-17 11:22:49 -0300

    Update app.js

--- item-service/src/app.js
+++ item-service/src/app.js
@@ -10,4 +10,16 @@ app.use(express.json());
 
 app.use('/items', itemRoutes);
 
+app.get('/',(req,res)=>{
+
+res.json({
+
+service:'GeekRent Item Service',
+
+status:'online'
+
+});
+
+});
+
 module.exports = app;
